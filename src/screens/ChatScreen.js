98e304880a9b313ae27a603af9268d9020ade544
Muhammed Ashik <ashik.muhammed@medleymed.com>
2024-01-05 13:12:55 +0530

    InputBox added

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,6 +1,8 @@
 import {
   FlatList,
   ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
   StyleSheet,
   Text,
   View,
@@ -8,17 +10,21 @@ import {
 import bg from "../../assets/images/BG.png";
 import Message from "../components/Message";
 import messages from "../../assets/data/messages.json";
+import InputBox from "../components/InputBox";
 const ChatScreen = () => {
-  return (
-    <ImageBackground source={bg} style={styles.bg}>
-      <FlatList
-        data={messages}
-        renderItem={({ item }) => <Message message={item} />}
-        style={{ padding: 10 }}
-        inverted
-      />
-    </ImageBackground>
-  );
+    return (
+      <KeyboardAvoidingView style={styles.bg} behavior={Platform.OS==="ios"?"padding":"height"}>
+        <ImageBackground source={bg} style={styles.bg}>
+          <FlatList
+            data={messages}
+            renderItem={({ item }) => <Message message={item} />}
+            style={{ padding: 10 }}
+            inverted
+          />
+          <InputBox />
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    );
 };
 
 const styles = StyleSheet.create({
